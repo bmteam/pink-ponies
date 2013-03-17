895999ac33158a9b9ad7803b61b9edf113cca238
Andrey Konovalov <adech.fo@gmail.com>
2013-03-17 16:33:25 +0400

    Added networking thread on client.

--- PinkPoniesApp/src/ru/pinkponiesapp/MainActivity.java
+++ PinkPoniesApp/src/ru/pinkponiesapp/MainActivity.java
@@ -1,23 +1,80 @@
 package ru.pinkponiesapp;
 
-import android.os.Bundle;
+import java.lang.ref.WeakReference;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private TextView textView;
+    private EditText editText;
+    
+    private NetworkingThread networkingThread;
+    
+    public Handler messageHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        textView = (TextView)findViewById(R.id.text_view);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        
+        editText = (EditText)findViewById(R.id.edit_message);
+        
+        messageHandler = new MessageHandler(this);
+        
+        networkingThread = new NetworkingThread(this);
+        networkingThread.start();
+        
+        printMessage("Initialized!");
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
     
+    public void onSendClick(View view) {
+        String message = editText.getText().toString();
+        editText.setText("");
+        sendMessageToNetworkingThread(message);
+    }
+    
+    private void onMessageFromNetworkingThread(String message) {
+        printMessage("NT: " + message);
+    }
+    
+    private void printMessage(String message) {
+    	textView.append(message + "\n");
+    }
+    
+    private void sendMessageToNetworkingThread(String message) {
+        Message msg = networkingThread.messageHandler.obtainMessage();
+        msg.obj = message;
+        networkingThread.messageHandler.sendMessage(msg);
+    }
+    
+    static public class MessageHandler extends Handler {
+        private WeakReference<MainActivity> activity;
+        
+        MessageHandler(MainActivity mainActivity) {
+            activity = new WeakReference<MainActivity>(mainActivity);
+        }
+        
+        @Override
+        public void handleMessage(Message msg) {
+            activity.get().onMessageFromNetworkingThread((String)msg.obj);
+        }
+    };
+
 }
---
+++ PinkPoniesApp/src/ru/pinkponiesapp/NetworkingThread.java
@@ -0,0 +1,56 @@
+package ru.pinkponiesapp;
+
+import java.lang.ref.WeakReference;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+
+public class NetworkingThread extends Thread {
+    //private final String serverIp = "192.168.0.198";
+    //private final int serverPort = 4263;
+    
+    private WeakReference<MainActivity> mainActivity;
+    
+    public MessageHandler messageHandler;
+    
+    NetworkingThread(MainActivity activity) {
+        mainActivity = new WeakReference<MainActivity>(activity);
+    }
+    
+    public void run() {
+        try {
+            Looper.prepare();
+            messageHandler = new MessageHandler(this);
+            Looper.loop();
+            
+            sendMessageToUIThread("Hello from networking thread!");
+            
+        } catch (Exception e) {
+            sendMessageToUIThread("Exception: " + e.getMessage());
+        }
+    }
+    
+    private void onMessageFromUIThread(String message) {
+        sendMessageToUIThread("Got your message: '" + message + "'!");
+    }
+    
+    private void sendMessageToUIThread(String message) {
+        Message msg = mainActivity.get().messageHandler.obtainMessage();
+        msg.obj = message;
+        mainActivity.get().messageHandler.sendMessage(msg);
+    }
+    
+    static public class MessageHandler extends Handler {
+        private WeakReference<NetworkingThread> thread;
+        
+        MessageHandler(NetworkingThread networkingThread) {
+            thread = new WeakReference<NetworkingThread>(networkingThread);
+        }
+        
+        @Override
+        public void handleMessage(Message msg) {
+            thread.get().onMessageFromUIThread((String)msg.obj);
+        }
+    };
+}
